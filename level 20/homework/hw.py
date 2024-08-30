f71b167e01fa2d8269017d80ce1290c0d81b4916
akaki1818 <qartlelaakaki@gmail.com>
2024-08-30 13:58:56 -0700

    second commit

---
+++ level 20/homework/hw.py
@@ -0,0 +1,27 @@
+
+
+sum_of_multiples = 0 
+
+for i in range(1, 101):  
+    if i % 5 == 0:  
+        sum_of_multiples += i  
+print("1-დან 100-მდე ხუთის ჯერადი რიცხვების ჯამი არის:", sum_of_multiples)  
+
+
+
+for i in range(1, 101): 
+    if i % 2 == 0: 
+        print(i, "ლუწია")  
+
+
+for i in range(1, 101): 
+    if i % 2 != 0:  
+        print(i, "კენტია")  
+
+
+
+sum_of_multiples = 0 
+for i in range(1, 101):  
+    if i % 5 == 0: 
+        sum_of_multiples += i  
+print("1-დან 100-მდე ხუთის ჯერადი რიცხვების ჯამი არის:", sum_of_multiples)
