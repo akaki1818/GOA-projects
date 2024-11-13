1e77b66d08d20e7e6293f0b0e3a0de267261e556
akaki1818 <qartlelaakaki@gmail.com>
2024-11-13 09:30:07 -0800

    Add files via upload

---
+++ level 20/classwork/cs.py
@@ -0,0 +1,14 @@
+total = 0
+for i in range(5, 101, 5):
+    total += i
+print(total)
+
+for i in range(2, 101, 2):
+    print(i, "არის ლუწი")
+
+
+    total = 0
+for i in range(1, 101):
+    if i % 5 == 0:
+        total += i 
+print(total)
