a7c95b33aee5d74a80078ec0d1e1f32d8aae53ef
akaki1818 <qartlelaakaki@gmail.com>
2024-11-14 09:05:11 -0800

    Add files via upload

---
+++ level 28/homework/hw.py
@@ -0,0 +1,22 @@
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for ricxvebi in numbers:
+    if ricxvebi % 2 == 0:
+        print(str(ricxvebi) +  "luwia")
+    else:
+        print(str(ricxvebi) + "kentia")
+
+for i in range(3, 31, 3):
+    print(i)
+
+numbers = [12, 5, 8, 15, 3, 9, 22, 6, 17, 1]
+for number in numbers:
+    for i in range(1, number):
+        print(numbers)
+
+#numberso = [12,100,1230,112,2,5,7,8,20,120,20,18,23,10]
+#if numberso > 20:
+    #print(numberso)
+
+    names = ["ნიკა", "მარიამი", "თედო", "გიორგი", "ანა"]
+    for name in names:
+        print(name + str(len(name)) + " ასო")
